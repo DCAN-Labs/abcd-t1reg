b0297ffceebaa7e566e43a984cb1cdcb3a7d3bc0
Anders Perrone <perronea@ohsu.edu>
2019-03-29 18:18:22 -0700

    Update terms and disable signups

--- src/components/Terms.vue
+++ src/components/Terms.vue
@@ -1,6 +1,32 @@
 <template name="terms">
   <div id="terms">
-    The data within this application is a part of the ABCD study. Approved users agree that they have a valid ABCD Data Use Agreement both at their site and for their person. Data within this app should not be used outside of the scope of the ABCD DUA.
+    <h4 style="text-align: center">
+      CONSENT FORM
+    </h4>
+    <h5 style="text-align: center">
+      Swipes for Science Brain Quality Control App
+    </h5>
+    <br>
+    <p>
+Welcome to the Swipes for Science Brain QC App! We're very excited to have you join us as we quality assess thousands of ABCD brain images in this fast, fun, and streamlined application. Please carefully read our Data Use Agreement before you begin your training session.
+    </p>
+    <br>
+    <p style="text-align: left">
+    1.  The data within this application is a part of the ABCD study. Approved users agree that they have a valid ABCD Data Use Agreement both at their site and for their person.
+    </p>
+    <p style="text-align: left">
+    2.  By signing up for the Swipes for Science application, you agree that you have been given explicit permission to participate in the quality assessment process by your supervisor, home institution, or other principal ABCD entity.
+    </p>
+    <p style="text-align: left">
+    3.  Data being used within this application is for the explicit purpose of brain imaging quality assessment as defined by the training criteria within the application.
+    </p>
+    <p style="text-align: left">
+    4.  These data will not be duplicated, photographed/recorded, or otherwise documented or shared externally unless at the permission or request of the Swipes for Science team.
+    </p>
+    <p style="text-align: left">
+    5.  Data within this application have been stripped of all identifiable information and will not be used in an attempt to establish individual identities from the study.
+    </p>
+
   </div>
 </template>
 <style>
